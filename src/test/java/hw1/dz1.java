ff39f22812afcee9779e9a2a98c18fcd512061e6
Guzeliya <uguzelia09@gmail.com>
2023-01-22 16:20:29 +0400

    dz

---
+++ src/test/java/hw1/dz1.java
@@ -0,0 +1,18 @@
+package hw1;
+
+
+
+public  class dz1 {
+    public static void main(String[] args) {
+        int[] arr = {10, 6, 87, 8, 9, 4, 11};
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+            min = Math.min(min, arr[i]);
+        }
+        System.out.println("Max = " + max);
+        System.out.println("Min = " + min);
+    }
+}
